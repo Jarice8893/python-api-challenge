e51c21079d0a805ec7f091d0f0b3ca0c8b32872a
Jarice Marshall <jmarshall@homeaccess.com>
2021-05-27 18:02:40 -0500

    homework

---
+++ api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+
+#weather_api_key= api.openweathermap.org/data/2.5/find?lat={lat}&lon={lon}&cnt={cnt}&appid={fa78039af46aabd0a5a216dbe90f1919}
+
+#weather_api_key= fa78039af46aabd0a5a216dbe90f1919
+
+weather_api_key= "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=fa78039af46aabd0a5a216dbe90f1919"
+
