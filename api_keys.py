713762696b010722ea4b4d4248dbbeef9db73f61
Jarice Marshall <jmarshall@homeaccess.com>
2021-06-02 03:22:38 -0500

    HOMEWOOORK

--- api_keys.py
+++ api_keys.py
@@ -2,7 +2,10 @@
 
 #weather_api_key= api.openweathermap.org/data/2.5/find?lat={lat}&lon={lon}&cnt={cnt}&appid={fa78039af46aabd0a5a216dbe90f1919}
 
-#weather_api_key= fa78039af46aabd0a5a216dbe90f1919
+weather_api_key= "fa78039af46aabd0a5a216dbe90f1919"
 
-weather_api_key= "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=fa78039af46aabd0a5a216dbe90f1919"
+#weather_api_key= "api.openweathermap.org/data/2.5/weather?id=524901&appid=fa78039af46aabd0a5a216dbe90f1919"
+
+#weather_api_key= "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=fa78039af46aabd0a5a216dbe90f1919"
+
 
